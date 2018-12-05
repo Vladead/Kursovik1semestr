192238e0a94128dbbbb6027604c6db4ff87d9d97
Vladead <deadjke1@gmail.com>
2018-12-05 19:51:08 +0300

    Добавление проверки файла, точек и радиуса в отдельной функции

--- main.cpp
+++ main.cpp
@@ -5,37 +5,54 @@
 using namespace std;
 
 int vvod(int stolbec, int stroka, double **ptrarr, int w);
-int proverka(int w);
+int proverka();
 
 int main() {
-    int prover = 0, stroka = 0, stolbec = 0, w = 0;
-    prover = proverka(w);
-    if (prover != 0) {
+    fstream f;
+    int prover = 0, tochek = 0;
+    double radius = 0;
+    prover = proverka();
+    if (prover != 0)
         return 666;
-    }
 
+    f.open("../cmake-build-debug/in.txt", ios::in);
+    f >> tochek >> radius;
+    tochek = abs(tochek);
+
+
+    auto **ptrarr = new double *[tochek];
+    for (int i = 0; i <= tochek - 1; i++)
+        ptrarr[i] = new double[2];
+
+    f.close();
+    delete[] ptrarr;
     return 0;
 }
 
-int proverka(int w) {
+int proverka() {
+    //----------------------------------------------------Проверяет радиус, точки и сам файл
     fstream f;
-
+    int tochek;
+    double radius = 0;
     f.open("../cmake-build-debug/in.txt", ios::in);
     if (f.bad() != 0) {
         cout << "Что-то пошло не так" << endl;
         return 666;
     }
-//    else {
-//        f >> stroka >> stolbec;
-//        auto **ptrarr = new double *[stroka];
-//        for (int i = 0; i <= stolbec - 1; i++)
-//            ptrarr[i] = new double[stolbec];
-//        if (stolbec <= 0 || stroka <= 0) {
-//            cout << "\nКоличество элементов не может быть нулевым или отрицательным!" << endl;
-//            return 666;
-//        }
-//        f.close();
-//    }
+    if (f.bad() == 0) {
+        f >> tochek >> radius;
+        if (radius <= 0) {
+            cout << "Радиус нуль или меньше, так нельзя!" << endl;
+            return 666;
+        }
+        if (abs(tochek) < 3) {
+            cout << "Маловато точек. Нужно минимум три" << endl;
+            return 666;
+        }
+        if (tochek < 0 && (abs(tochek) >= 3))
+            cout << "\nВы ввели отрицательное число точек. Я буду считать, что минус вы поставили по ошибке" << endl;
+    }
+    f.close();
     return 0;
 }
 int vvod(int stolbec, int stroka, double **ptrarr, int w) {
